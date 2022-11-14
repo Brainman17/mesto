e6db81d4eca614153f6d714ff0ab23d1c1f02b5c
Egor Andreychuk <jegor.andreichuk@yandex.ru>
2022-11-14 18:44:02 +0300

    сделать проект

---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+const popupElement = document.querySelector('.popup');
+const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
+const popupEditButtonElement = document.querySelector('.profile__edit-button');
+const popupSaveButtonElement = popupElement.querySelector('.popup__save-button');
+
+popupEditButtonElement.addEventListener('click', function() {
+  popupElement.classList.add('popup_opened');
+});
+
+popupCloseButtonElement.addEventListener('click', function() {
+  popupElement.classList.remove('popup_opened');
+})
+
+popupSaveButtonElement.addEventListener('click', function() {
+  popupElement.classList.remove('popup_opened');
+})
+
+const formElement = popupElement.querySelector('.popup__form-content');
+const nameInput = formElement.querySelector('.popup__name');
+const jobInput = formElement.querySelector('.popup__job');
+const profileName = document.querySelector('.profile__title');
+const profileJob = document.querySelector('.profile__subtitle');
+
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
