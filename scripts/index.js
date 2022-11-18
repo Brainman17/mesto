805076248b2809c26d62a8c4bac37620094882bf
Egor Andreychuk <jegor.andreichuk@yandex.ru>
2022-11-19 02:13:16 +0300

    fix: подправить js

--- scripts/index.js
+++ scripts/index.js
@@ -2,30 +2,30 @@ const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
 const popupEditButtonElement = document.querySelector('.profile__edit-button');
 const popupSaveButtonElement = popupElement.querySelector('.popup__save-button');
-
-popupEditButtonElement.addEventListener('click', function() {
-  popupElement.classList.add('popup_opened');
-});
-
-popupCloseButtonElement.addEventListener('click', function() {
-  popupElement.classList.remove('popup_opened');
-})
-
-popupSaveButtonElement.addEventListener('click', function() {
-  popupElement.classList.remove('popup_opened');
-})
-
 const formElement = popupElement.querySelector('.popup__form-content');
 const nameInput = formElement.querySelector('.popup__form-subtitle_value_name');
 const jobInput = formElement.querySelector('.popup__form-subtitle_value_job');
 const profileName = document.querySelector('.profile__title');
 const profileJob = document.querySelector('.profile__subtitle');
 
+function popupOpen() {
+  popupElement.classList.add('popup_opened');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+}
+function popupClose() {
+  popupElement.classList.remove('popup_opened');
+  nameInput.value = "";
+  jobInput.value = "";
 
+}
 function formSubmitHandler (evt) {
     evt.preventDefault();
+  popupElement.classList.remove('popup_opened');
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
 }
 
+popupEditButtonElement.addEventListener('click', popupOpen);
+popupCloseButtonElement.addEventListener('click', popupClose);
 formElement.addEventListener('submit', formSubmitHandler);
