ff546ad18b8b733bcb8d17a0ff045ec4766be6e3
Egor Andreychuk <jegor.andreichuk@yandex.ru>
2022-11-29 14:05:16 +0300

    ...

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,18 @@
 import {initialCards} from './array.js';
 
 
-
 const popupElement = document.querySelector('.popup');
 const popupAdd = document.querySelector('.popup_add');
 const popupEdit = document.querySelector('.popup_edit');
+const popupCard= document.querySelector('.popup_card');
 const popupAddButtonElement = document.querySelector('.profile__add-button');
-const popupCloseButtonElement = popupElement.querySelectorAll('.popup__close-button');
+const popupCloseButtonElement = document.querySelectorAll('.popup__close-button');
 const popupEditButtonElement = document.querySelector('.profile__edit-button');
-const popupSaveButtonElement = popupElement.querySelector('.popup__save-button');
 const formElement = popupElement.querySelector('.popup__form-content');
 const nameInput = formElement.querySelector('.popup__form-subtitle_value_name');
 const jobInput = formElement.querySelector('.popup__form-subtitle_value_job');
-const titleInput = formElement.querySelector('.popup__form-subtitle_value_title');
-const linkInput = formElement.querySelector('.popup__form-subtitle_value_link');
 const profileName = document.querySelector('.profile__title');
 const profileJob = document.querySelector('.profile__subtitle');
-const titleCard = document.querySelector('.card__subtitle');
-const linkCard = document.querySelector('card__image');
 
 function popupEditOpen() {
   popupEdit.classList.add('popup_opened');
@@ -32,6 +27,7 @@ function popupAddOpen() {
 function popupClose() {
   popupEdit.classList.remove('popup_opened');
   popupAdd.classList.remove('popup_opened');
+  popupCard.classList.remove('popup_opened');
 }
 
 
@@ -52,7 +48,6 @@ formElement.addEventListener('submit', formSubmitHandler);
 
 // Практическая работа 5
 
-
 const cardsList = document.querySelector('.cards');
 const cardTemplate = document.querySelector('#card-template').content.querySelector('.card');
 const form = document.querySelector('.popup__form-content_add');
@@ -60,22 +55,49 @@ const formInputName = document.querySelector('[name="name-add"]');
 const formInputLink = document.querySelector('[name="link-add"]');
 
 
+
+// Создание карточек
+
 function createElement(item) {
   const card = cardTemplate.cloneNode(true);
   const cardTitle = card.querySelector('.card__subtitle');
   const DeleteButton = card.querySelector('.card__delete-button');
   const LikeButton = card.querySelector('.card__heart-button');
   const img = card.querySelector('.card__image');
+  const popupCardSubtitle = document.querySelector('.popup__card-subtitle');
+  const popupCardImage = document.querySelector('.popup__card-img');
 
   img.src = item.link;
   cardTitle.textContent = item.name;
+  popupCardImage.src = img.src;
+  popupCardSubtitle.textContent = cardTitle.textContent;
 
   DeleteButton.addEventListener('click', DeleteButtonClick);
   LikeButton.addEventListener('click', LikeButtonClick);
+  img.addEventListener('click', openCardClick);
 
   return card;
 
 }
+
+// Обработчик событий
+
+const formCreateHandler = (evt) => {
+  evt.preventDefault();
+
+  renderCard({ name: formInputName.value,
+    link: formInputLink.value});
+
+  popupAdd.classList.remove('popup_opened');
+
+}
+
+const renderCard = (item) => {
+  cardsList.prepend(createElement(item));
+};
+
+form.addEventListener('submit', formCreateHandler);
+
 
 const DeleteButtonClick = (e) => {
   e.target.closest('.card').remove();
@@ -85,21 +107,28 @@ const LikeButtonClick = (e) => {
   e.target.classList.toggle('card__heart-button_active');
 }
 
-initialCards.forEach(function(item) {
+const openCardClick = (e) => {
+  e.target.classList.add('popup_card');
+ }
+
+// Рендер карточек
+
+
+initialCards.forEach((item) => {
   const element = createElement(item);
   cardsList.append(element);
 })
 
-const formCreateHandler = (evt) => {
-  evt.preventDefault();
-  const list = {
-    name: formInputName.value,
-    link: formInputLink.value
-  }
 
-  const element = createElement(list);
-  cardsList.append(element);
 
-}
 
-form.addEventListener('submit', formCreateHandler);
+
+
+
+
+
+
+
+
+
+
