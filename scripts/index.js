4f6e1cf139b0b38488042c5363406e56cc8525d9
Egor Andreychuk <jegor.andreichuk@yandex.ru>
2022-12-01 02:01:38 +0300

    feature: сделать корректное отображение картинок в попапе

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,60 @@
 import {initialCards} from './array.js';
 
+// Переменные
 
-const popupElement = document.querySelector('.popup');
+
 const popupAdd = document.querySelector('.popup_add');
 const popupEdit = document.querySelector('.popup_edit');
 const popupCard= document.querySelector('.popup_card');
 const popupAddButtonElement = document.querySelector('.profile__add-button');
-const popupCloseButtonElement = document.querySelectorAll('.popup__close-button');
+const popupCloseButtonElementList = document.querySelectorAll('.popup__close-button');
 const popupEditButtonElement = document.querySelector('.profile__edit-button');
-const formElement = popupElement.querySelector('.popup__form-content');
-const nameInput = formElement.querySelector('.popup__form-subtitle_value_name');
-const jobInput = formElement.querySelector('.popup__form-subtitle_value_job');
+const formElementEdit = document.querySelector('.popup__form-content_edit');
+const nameInput = formElementEdit.querySelector('.popup__form-subtitle_value_name');
+const jobInput = formElementEdit.querySelector('.popup__form-subtitle_value_job');
 const profileName = document.querySelector('.profile__title');
 const profileJob = document.querySelector('.profile__subtitle');
+const cardsContainer = document.querySelector('.cards');
+const cardTemplate = document.querySelector('#card-template').content.querySelector('.card');
+const formElementAdd = document.querySelector('.popup__form-content_add');
+const formInputName = document.querySelector('[name="name-add"]');
+const formInputLink = document.querySelector('[name="link-add"]');
+const popupCardSubtitle = document.querySelector('.popup__card-subtitle');
+const popupCardImage = document.querySelector('.popup__card-img');
+
+const handleCreateButton = (evt) => {
+  evt.preventDefault();
+
+  renderCard({ name: formInputName.value,
+    link: formInputLink.value});
+
+  evt.target.reset();
+
+  closePopup(popupAdd);
+
+}
+
+const renderCard = (item) => {
+  cardsContainer.prepend(createElement(item));
+};
+
+const openCardClick = (e) => {
+  popupCardImage.src = e.target.currentSrc;
+  popupCardImage.alt = e.target.parentNode.innerText;
+  popupCardSubtitle.textContent = e.target.parentNode.innerText;
+  popupCard.classList.add('popup_opened');
+}
+
+const deleteButtonClick = (e) => {
+  e.target.closest('.card').remove();
+}
+
+const likeButtonClick = (e) => {
+  e.target.classList.toggle('card__heart-button_active');
+}
+
+// Функции
+
 
 function popupEditOpen() {
   popupEdit.classList.add('popup_opened');
@@ -22,6 +64,18 @@ function popupEditOpen() {
 
 function popupAddOpen() {
   popupAdd.classList.add('popup_opened');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+}
+
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+
+}
+
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+
 }
 
 function popupClose() {
@@ -30,95 +84,48 @@ function popupClose() {
   popupCard.classList.remove('popup_opened');
 }
 
-
-function formSubmitHandler (evt) {
-    evt.preventDefault();
-  popupEdit.classList.remove('popup_opened');
+function handleSubmitButton (evt) {
+  evt.preventDefault();
+  closePopup(popupEdit);
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
 }
 
-popupAddButtonElement.addEventListener('click', popupAddOpen);
-popupEditButtonElement.addEventListener('click', popupEditOpen);
-popupCloseButtonElement.forEach(btn => btn.addEventListener('click', popupClose));
-formElement.addEventListener('submit', formSubmitHandler);
-
-
-
-const cardsList = document.querySelector('.cards');
-const cardTemplate = document.querySelector('#card-template').content.querySelector('.card');
-const form = document.querySelector('.popup__form-content_add');
-const formInputName = document.querySelector('[name="name-add"]');
-const formInputLink = document.querySelector('[name="link-add"]');
-
-
-
-// Создание карточек
-
 function createElement(item) {
   const card = cardTemplate.cloneNode(true);
   const cardTitle = card.querySelector('.card__subtitle');
-  const DeleteButton = card.querySelector('.card__delete-button');
-  const LikeButton = card.querySelector('.card__heart-button');
+  const deleteButton = card.querySelector('.card__delete-button');
+  const likeButton = card.querySelector('.card__heart-button');
   const img = card.querySelector('.card__image');
 
   img.src = item.link;
+  img.alt = item.name;
   cardTitle.textContent = item.name;
 
-  DeleteButton.addEventListener('click', DeleteButtonClick);
-  LikeButton.addEventListener('click', LikeButtonClick);
+  deleteButton.addEventListener('click', deleteButtonClick);
+  likeButton.addEventListener('click', likeButtonClick);
   img.addEventListener('click', openCardClick);
 
   return card;
 
 }
 
-// Обработчик событий
-
-const formCreateHandler = (evt) => {
-  evt.preventDefault();
-
-  renderCard({ name: formInputName.value,
-    link: formInputLink.value});
-
-  popupAdd.classList.remove('popup_opened');
-
-}
-
-const renderCard = (item) => {
-  cardsList.prepend(createElement(item));
-};
-
-form.addEventListener('submit', formCreateHandler);
+// Обработчики событий
 
 
-
-const openCardClick = (e) => {
-  const popupCardSubtitle = document.querySelector('.popup__card-subtitle');
-  const popupCardImage = document.querySelector('.popup__card-img');
-  popupCardImage.src = e.target.currentSrc;
-  popupCardSubtitle.textContent = e.target.parentNode.innerText;
-  popupCard.classList.add('popup_opened');
-}
-
-const DeleteButtonClick = (e) => {
-  e.target.closest('.card').remove();
-}
-
-const LikeButtonClick = (e) => {
-  e.target.classList.toggle('card__heart-button_active');
-}
-
-
+popupAddButtonElement.addEventListener('click', popupAddOpen);
+popupEditButtonElement.addEventListener('click', popupEditOpen);
+popupCloseButtonElementList.forEach(btn => btn.addEventListener('click', popupClose));
+formElementEdit.addEventListener('submit', handleSubmitButton);
+formElementAdd.addEventListener('submit', handleCreateButton);
 
 // Рендер карточек
 
 
 initialCards.forEach((item) => {
   const element = createElement(item);
-  cardsList.append(element);
+  cardsContainer.append(element);
 })
-
 
 
 
